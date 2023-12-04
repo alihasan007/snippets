1957b722e3199a7eaf70e6c8ade812b948695d71
syed ali hasan <syedalihasan10@gmai.com>
2023-12-04 15:16:16 +0530

    add "useFormhook" for validation

--- src/app/snippets/new/page.tsx
+++ src/app/snippets/new/page.tsx
@@ -1,31 +1,13 @@
-import { redirect } from "next/navigation";
-import { db } from "@/db";
+"use client";
+import { createSnippet } from "@/actions";
+import { useFormState } from "react-dom";
 
 const SnippetCreatePage = () => {
-  async function createSnippet(formData: FormData) {
-    // needs to be server actions
-
-    "use server";
-    // check the user input make sure they are filled
-
-    const title = formData.get("title") as string;
-    const code = formData.get("code") as string;
-
-    // Create a new record in DB
-    const snippet = await db.snippet.create({
-      data: {
-        title,
-        code,
-      },
-    });
-    console.log(snippet);
-    //Redirect user to root route
-    redirect("/");
-  }
+  const [formState, action] = useFormState(createSnippet, { message: "" });
 
   return (
     <div>
-      <form action={createSnippet}>
+      <form action={action}>
         <h3 className="font-bold m-3">Create a Snippet</h3>
         <div className="flex flex-col gap-4">
           <div className="flex gap-4">
@@ -48,6 +30,11 @@ const SnippetCreatePage = () => {
               id="code"
             />
           </div>
+          {formState.message ? (
+            <div className="my-2 p-2 bg-red-200 border rounded border-red-400">
+              {formState.message}
+            </div>
+          ) : null}
           <button type="submit" className=" rounded p-2 bg-blue-200">
             Create
           </button>
